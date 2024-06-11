26c03754403de20dd29dfc809799bc945ed68a25
Victor Kovalchuk <victorkovalchuk@Victors-MacBook-Air.local>
2024-06-11 22:59:37 +0300

    bug fixing; improved header

--- assets/header.js
+++ assets/header.js
@@ -1,5 +1,7 @@
-document.querySelectorAll('button[header-link-with-child="true"]').forEach((button) => {
-  button.addEventListener('click', () => {
-    button.nextElementSibling.classList.toggle('hidden');
-  });
+document.querySelector('button.hamburger').addEventListener('click', () => {
+  document.querySelector('.side-menu').classList.remove('hidden');
+});
+
+document.querySelector('button.close-side-menu ').addEventListener('click', () => {
+  document.querySelector('.side-menu').classList.add('hidden');
 });
