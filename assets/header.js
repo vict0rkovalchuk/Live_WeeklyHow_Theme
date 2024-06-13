0b668d26dc9625ba2bddd180bba40716bc4f6d93
Victor Kovalchuk <victorkovalchuk@Victors-MacBook-Air.local>
2024-06-13 11:19:33 +0300

    minor improvements in header.liquid

--- assets/header.js
+++ assets/header.js
@@ -2,6 +2,8 @@ document.querySelector('button.hamburger').addEventListener('click', () => {
   document.querySelector('.side-menu').classList.remove('hidden');
 });
 
-document.querySelector('button.close-side-menu ').addEventListener('click', () => {
-  document.querySelector('.side-menu').classList.add('hidden');
-});
+document
+  .querySelector('button.close-side-menu ')
+  .addEventListener('click', () => {
+    document.querySelector('.side-menu').classList.add('hidden');
+  });
